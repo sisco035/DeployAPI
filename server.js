76c8d5c0da9b7d60afb90d0ab979b429dbee5f97
Francisco <sisco035@gmail.com>
2018-10-14 16:36:59 -0400

    coimmit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const http = require('http');
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+app.use(express.static('public'))
+
+server.listen(port);
